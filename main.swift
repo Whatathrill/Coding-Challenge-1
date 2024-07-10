9c570eab230ef646a32ee3d6433c83af7158f888
Dorian Lang <87432133+Whatathrill@users.noreply.github.com>
2024-07-10 15:10:07 -0400

    Create main.swift2

---
+++ main.swift
@@ -0,0 +1,22 @@
+var appname:String = "Snapchat"
+var devname:String="Dorian Lang"
+var releaseyear:Int=2011
+var appversion:Double=1.0
+var numberofdownloads:Int=74
+var rating:Double=4.2
+print(appname)
+print(devname)
+print(releaseyear)
+print(appversion)
+print(rating)
+appversion+=0.1
+numberofdownloads+=1000
+rating+=0.2
+print("The name of this app is \(appname)")
+print("The developer of this app is \(devname)")
+print("This app was released in \(releaseyear)")
+print("App version: \(appversion)")
+print("This app is rated \(appname)")
+
+
+
